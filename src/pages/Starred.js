8a6e1c4041366a839290e9cabe1455033235945a
NikhilGupta <nikhilg398@gmail.com>
2021-07-29 00:54:01 +0530

    added layout management

---
+++ src/pages/Starred.js
@@ -0,0 +1,9 @@
+/* eslint-disable arrow-body-style */
+import React from 'react';
+import MainPageLayout from '../components/MainPageLayout';
+
+const Starred = () => {
+  return <MainPageLayout>This is Starred page</MainPageLayout>;
+};
+
+export default Starred;
