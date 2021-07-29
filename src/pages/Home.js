df13615f8fa8240ebf8cc378eac9e6164c84bf66
NikhilGupta <nikhilg398@gmail.com>
2021-07-29 16:21:07 +0530

    added tv maze api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,43 @@
-import React from 'react';
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+/* eslint-disable arrow-body-style */
+import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
 
-const Home = () => <MainPageLayout> This is home page </MainPageLayout>;
+const Home = () => {
+  const [input, setInput] = useState('');
+
+  const onSearch = () => {
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+      .then(r => r.json())
+      .then(result => {
+        console.log(result);
+      });
+  };
+
+  const onInputChange = ev => {
+    setInput(ev.target.value);
+  };
+
+  const onKeyDown = ev => {
+    if (ev.keyCode === 13) {
+      onSearch();
+    }
+  };
+
+  return (
+    <MainPageLayout>
+      <input
+        type="text"
+        onChange={onInputChange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
+    </MainPageLayout>
+  );
+};
 
 export default Home;
