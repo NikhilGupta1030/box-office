0be05b7200bce304561cfc05403dd27288e3bf74
NikhilGupta <nikhilg398@gmail.com>
2021-08-08 14:07:47 +0530

    created preview cards

---
+++ src/components/actor/ActorGrid.js
@@ -0,0 +1,26 @@
+/* eslint-disable react/self-closing-comp */
+/* eslint-disable arrow-body-style */
+import React from 'react';
+import ActorCard from './ActorCard';
+
+import IMAGE_NOT_FOUND from '../../images/not-found.png';
+
+const ActorGrid = ({ data }) => {
+  return (
+    <div>
+      {data.map(({ person }) => (
+        <ActorCard
+          key={person.id}
+          name={person.name}
+          country={person.country ? person.country.name : null}
+          birthday={person.birthday}
+          deathday={person.deathday}
+          gender={person.gender}
+          image={person.image ? person.image.medium : IMAGE_NOT_FOUND}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default ActorGrid;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,33 +2,22 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable arrow-body-style */
 import React, { useState } from 'react';
+import ActorGrid from '../components/actor/ActorGrid';
 import MainPageLayout from '../components/MainPageLayout';
+import ShowGrid from '../components/show/ShowGrid';
 import { apiGet } from '../misc/config';
 
 const Home = () => {
   const [input, setInput] = useState('');
   const [results, setResults] = useState(null);
+  const [searchOption, setSearchOption] = useState('shows');
+
+  const isShowsSearch = searchOption === 'shows';
 
   const onSearch = () => {
-    apiGet(`/search/shows?q=${input}`).then(result => {
+    apiGet(`/search/${searchOption}?q=${input}`).then(result => {
       setResults(result);
     });
-  };
-
-  const renderResults = () => {
-    if (results && results.length === 0) {
-      return <div>No results</div>;
-    }
-    if (results && results.length > 0) {
-      return (
-        <div>
-          {results.map(item => (
-            <div key={item.show.id}>{item.show.name}</div>
-          ))}
-        </div>
-      );
-    }
-    return null;
   };
 
   const onInputChange = ev => {
@@ -41,14 +30,57 @@ const Home = () => {
     }
   };
 
+  const onRadioChange = ev => {
+    setSearchOption(ev.target.value);
+  };
+
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div>No results</div>;
+    }
+    if (results && results.length > 0) {
+      return results[0].show ? (
+        <ShowGrid data={results} />
+      ) : (
+        <ActorGrid data={results} />
+      );
+    }
+    return null;
+  };
+
   return (
     <MainPageLayout>
       <input
         type="text"
+        placeholder="Search for something"
         onChange={onInputChange}
         onKeyDown={onKeyDown}
         value={input}
       />
+
+      <div>
+        <label htmlFor="shows-search">
+          Shows
+          <input
+            id="shows-search"
+            type="radio"
+            value="shows"
+            checked={isShowsSearch}
+            onChange={onRadioChange}
+          />
+        </label>
+        <label htmlFor="actors-search">
+          Actors
+          <input
+            id="actors-search"
+            type="radio"
+            value="people"
+            checked={!isShowsSearch}
+            onChange={onRadioChange}
+          />
+        </label>
+      </div>
+
       <button type="button" onClick={onSearch}>
         Search
       </button>
